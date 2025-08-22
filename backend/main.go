9d1918aad65d442e702f83b045e9c2c9acc586c2
Antony <antonymurithi51@gmail.com>
2025-08-23 00:07:21 +0300

    authenticated user can be able to login through the use of cookie

--- backend/main.go
+++ backend/main.go
@@ -4,12 +4,18 @@ import (
 	"github.com/codewithAntony/cliently4us/database"
 	"github.com/codewithAntony/cliently4us/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	database.Connect()
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:5173",
+		AllowCredentials: true,
+	}))
 
 	routes.Setup(app)
 
